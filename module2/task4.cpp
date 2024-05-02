577a0fae5e2ae61a1f4bb74d38fd45f055e84edf
dmitry <dkoshenkofff@mail.ru>
2024-05-03 01:39:39 +0300

    idk

--- module2/task4.cpp
+++ module2/task4.cpp
@@ -13,6 +13,7 @@ struct Node {
     Node *left;
     Node *right;
 
+    size_t count{};
     uint8_t height{};
 
     Node() = default;
@@ -22,7 +23,7 @@ struct Node {
         delete right;
     }
 
-    explicit Node(T _key) : key(_key), left(nullptr), right(nullptr), height(0) {}
+    explicit Node(T _key) : key(_key), left(nullptr), right(nullptr), height(0), count(0) {}
 };
 
 template<typename T>
@@ -41,129 +42,149 @@ class avl_tree {
     Node<T> *root;
     Comparator cmp;
 
-    uint8_t get_height(const Node<T> *node) const {
-        return (node ? node->height : 0);
-    }
-
-    int bfactor(const Node<T> *node) const {
-        return get_height(node->left) - get_height(node->right);
-    }
-
-    void fix_height(Node<T> *node) {
-        node->height = std::max(get_height(node->left), get_height(node->right))+ 1;
-    }
-
-    void destroy(Node<T> *node) {
-        if (node) {
-            destroy(node->left);
-            destroy(node->right);
-            delete node;
-        }
-    }
-
-    Node<T> *_find(Node<T> *node, T value) {
-        if (node == nullptr)
-            return nullptr;
-        if (cmp(node->key, value) == 0)
-            return node;
-
-        if (cmp(value, node->key) < 0) {
-            _find(node->left, value);
-        } else {
-            _find(node->right, value);
-        }
-        return nullptr;
-    }
-
-    Node<T> *find_parent_min(Node<T> *node) {
-        return (node->left->left == nullptr) ? node : find_parent_min(node->left);
-    }
-
-    Node<T> *find(const T &value) {
-        Node<T> *node = root;
-        return _find(node, value);
-    }
-
-    // TODO: сделать
-    Node<T> *rotate_right(Node<T> *node) {
-
-    }
-
-    // TODO: сделать
-    Node<T> *rotate_left(Node<T> *node) {
-
-    }
-
-    Node<T>* balance(Node<T>* node){
-        fix_height(node);
-
-        auto bf = bfactor(node);
-        if (bf == 2){
-            if (bfactor(node->right) < 0){
-                node->righe = rotate_right(node->right);
-            }
-            return rotate_left(node);
-        }
-        if (bf == -2){
-            if (bfactor(node->left) > 0){
-                node->left = rotate_left(node->left);
-            }
-            return rotate_right(node);
-        }
-        return node;
-    }
 public:
-    avl_tree() = default;
+    avl_tree() : root(nullptr) {}
 
     ~avl_tree() {
         delete root;
     }
 
-    void add(const T &key) {
-
+    void insert(T &key) {
+        root = _insert(root, key);
     }
 
     T kth_stat(size_t k) {
-
-    }
-
-    bool has(const T &val) {
-        return find(val) != nullptr;
-    }
-
-    void del(const T &value) {
-        auto node = find(value);
-        if (node->left == nullptr) {
-            auto r = node->right;
-            delete node;
-
-            node = r;
-        } else if (node->right == nullptr) {
-            auto l = node->left;
-            delete node;
-            node = l;
-        } else {
-            auto parent_min = find_parent_min(node->right);
-            auto min = parent_min->left;
-
-            node->key = min->key;
-            (parent_min->left == min ? parent_min->left : parent_min->right) =
-                    min->right;
-            delete min;
+        auto node = root;
+        size_t left_count = get_count(node->left);
+        size_t a = 0, b = get_count(node);
+        while (k != a + left_count){
+            if (k < a + left_count){
+                b -= get_count(node->right);
+                node = node->left;
+            } else {
+                a += left_count + 1;
+                node = node->right;
+            }
+            left_count = get_count(node->left);
         }
+        return node->key;
+    }
+
+    void erase(const T &value) {
+        root = _erase(root, value);
+    }
+
+private:
+    Node<T>* _erase(Node<T>* node, const T& value){
+        if (!node)
+            return nullptr;
+
+        auto cmp_res = cmp(value, node->key);
+
+        if (cmp_res == -1)
+            node->left = _erase(node->left, value);
+        else if (cmp_res == 1)
+            node->right = _erase(node->right, value);
+        else {
+            Node<T>* left = node->left;
+            Node<T>* right = node->right;
+
+            delete node;
+            if (!right){
+                return left;
+            }
+            Node<T>* min_node = find_min(right);
+            right = remove_min_node(right);
+
+            min_node->left = left;
+            min_node->right = right;
+
+            return balance(min_node);
+        }
+        return balance(node);
+    }
+    Node<T>* remove_min_node(Node<T>* node) {
+        if (!node->left) {
+            return node->right;
+        }
+
+        node->left = remove_min_node(node->left);
+        return balance(node);
+    }
+
+    Node<T>* find_min(Node<T>* node) {
+        if (!node->left) {
+            return node;
+        }
+        return find_min(node->left);
+    }
+
+    Node<T>* _insert(Node<T>* node, T& value){
+        if (!node)
+            return new Node<T>(value);
+
+        if (value < node->key){
+            node->left = _insert(node->left, value);
+        }else{
+            node->right = _insert(node->right, value);
+        }
+        return balance(node);
+    }
+
+    Node<T>* balance(Node<T>* node){
+        fix(node);
+
+        auto bf = b_factor(node);
+        if (bf == 2){
+            if (b_factor(node->right) < 0)
+                node->right = rotate_right((node->right));
+            return rotate_left(node);
+        }
+        if (bf == -2){
+            if (b_factor(node->left) > 0)
+                node->left = rotate_left(node->left);
+            return rotate_right(node);
+        }
+        return node;
+    }
+
+    size_t get_height(Node<T>* node){
+        return ((node)? node->height : 0);
+    }
+
+    size_t get_count(Node<T>* node){
+        return ((node)? node->count : 0);
+    }
+
+    void fix(Node<T>* node){
+        node->height = std::max(get_height(node->left), get_height(node->right), cmp);
+        node->count = 1 + get_count(node->left) + get_height(node->right);
+    }
+
+    int b_factor(Node<T>* node){
+        return get_height(node->left) - get_height(node->right);
+    }
+
+    Node<T>* rotate_left(Node<T>* node){
+        Node<T>* ptr = node->right;
+        node->right = ptr->left;
+        ptr->left = node;
+
+        fix(ptr);
+        fix(node);
+        return ptr;
+    }
+
+    Node<T>* rotate_right(Node<T>* node){
+        Node<T>* ptr = node->left;
+        node->left = ptr->right;
+        ptr->right = node;
+
+        fix(node);
+        fix(ptr);
+        return ptr;
     }
 };
-
-template<typename T, class Comparator = DefaultComparator<T>>
-T abs_num(const T &num) {
-    Comparator cmp;
-    if (cmp(num, T(0)) < 0) {
-        return -1 * num;
-    }
-
-    return num;
-}
-
 
 void run(std::istream &in, std::ostream &out) {
     int N;
@@ -174,9 +195,10 @@ void run(std::istream &in, std::ostream &out) {
         int pos;
         in >> num >> pos;
         if (num > 0)
-            tree.add(num);
+            tree.insert(num);
         else
-            tree.del(abs_num(num));
+            tree.erase(-num);
+
         out << tree.kth_stat(pos);
     }
 }
