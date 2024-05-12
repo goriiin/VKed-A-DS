b57f538e089bc5f8bd72666bd8a5b44db05300f4
dmitry <dkoshenkofff@mail.ru>
2024-05-13 02:47:43 +0300

    3 fix

--- module2/task3.cpp
+++ module2/task3.cpp
@@ -1,78 +1,97 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 
-void run(std::istream &istream, std::ostream &ostream);
+
+void run(std::istream &in, std::ostream &out);
 
 int main() {
     run(std::cin, std::cout);
 }
 
 template<typename T>
-class b_tree {
+class BTree {
+public:
     struct Node {
-        bool leaf;
-        std::vector<T> keys;
-        std::vector<Node *> children;
-
-        Node() = default;
         Node(bool leaf) : leaf(leaf) {}
 
         ~Node() {
-            for (Node *ch: children) {
-                delete ch;
+            for (Node *child: children) {
+                delete child;
             }
         }
 
-        void resize(size_t _t) {
-            keys.resize(2 * _t -1);
-            children.resize(2 * _t);
-        }
+        bool leaf;
+        std::vector<T> keys;
+        std::vector<Node *> children;
     };
 
-    size_t t;
-    Node *root;
-public:
-    b_tree(size_t min_degree) : t(min_degree), root(nullptr) {}
+    BTree(size_t min_degree)
+            : root(nullptr), t(min_degree) {
+        assert(min_degree >= 2);
+    }
 
-    ~b_tree() {
+    ~BTree() {
         if (root)
             delete root;
     }
 
     void insert(const T &key) {
-        if (root == nullptr)
+        if (!root)
             root = new Node(true);
 
         if (is_node_full(root)) {
-            Node *new_root = new Node(false);
-            new_root->children.push_back(root);
-
-            split_child(new_root, 0, root);
-            root = new_root;
+            Node *newRoot = new Node(false);
+            newRoot->children.push_back(root);
+            root = newRoot;
+            split(root, 0);
         }
 
         insert_non_full(root, key);
     }
 
-    void print(std::ostream &out) {
-        _print(out,root);
-    }
+    void print(std::ostream&out) {
+        if (root == nullptr) return;
 
+        std::vector<Node *> currentLevel = {root};
+        while (!currentLevel.empty()) {
+            std::vector<Node *> nextLevel;
+            for (Node *node: currentLevel) {
+                for (auto &key: node->keys) {
+                    out << key << " ";
+                }
+
+                if (!node->leaf) {
+                    for (Node *child: node->children) {
+                        nextLevel.push_back(child);
+                    }
+                }
+            }
+            out << std::endl;
+            currentLevel = nextLevel;
+        }
+    }
 private:
-    void _print(std::ostream &out, Node *node) {
-        for (const auto & key : node->keys){
-            out << key << " ";
-        }
-        out << std::endl;
-
-        for (auto n: node->children) {
-            _print(out, n);
-        }
+    Node *root;
+    size_t t;
+    bool is_node_full(Node *node) {
+        return node->keys.size() == 2 * t - 1;
     }
 
-    bool is_node_full(Node *node) {
-        return node->keys.size() == 2*t - 1;
+    void split(Node *node, size_t index) {
+        Node *child = node->children[index];
+        Node *newChild = new Node(child->leaf);
+        node->children.insert(node->children.begin() + index + 1, newChild);
+        node->keys.insert(node->keys.begin() + index, child->keys[t - 1]);
+
+        newChild->keys.assign(child->keys.begin() + t, child->keys.end());
+        child->keys.resize(t - 1);
+
+        if (!child->leaf) {
+            newChild->children.assign(child->children.begin() + t, child->children.end());
+            child->children.resize(t);
+        }
     }
 
     void insert_non_full(Node *node, const T &key) {
@@ -81,50 +100,39 @@ private:
             node->keys.resize(node->keys.size() + 1);
             while (pos >= 0 && key < node->keys[pos]) {
                 node->keys[pos + 1] = node->keys[pos];
-                --pos;
+                pos--;
             }
             node->keys[pos + 1] = key;
         } else {
             while (pos >= 0 && key < node->keys[pos]) {
-                --pos;
+                pos--;
             }
-            if (is_node_full(node->children[pos])) {
-                split_child(node, pos, node->children[pos]);
-                if (key > node->keys[pos - 1]) {
-                    ++pos;
+
+            if (is_node_full(node->children[pos + 1])) {
+                split(node, pos + 1);
+                if (key > node->keys[pos + 1]) {
+                    pos++;
                 }
             }
-            insert_non_full(node->children[pos], key);
+            insert_non_full(node->children[pos + 1], key);
         }
     }
 
-    void split_child(Node *x, size_t index, Node* y) {
-        Node* z = new Node(y->leaf);
-        z->keys.assign(y->keys.begin() + t, y->keys.end());
-        if (!y->leaf) {
-            z->children.assign(y->children.begin() + t, y->children.end()); // Используем assign для копирования
-        }
 
-        y->keys.resize(t - 1);
-        y->children.resize(t);
-
-        x->children.insert(x->children.begin() + index + 1, z);
-        x->keys.insert(x->keys.begin() + index, y->keys[t - 1]);
-    }
 };
+
 
 void run(std::istream &in, std::ostream &out) {
     int t;
     in >> t;
-    b_tree<int> btree(t);
+    BTree<int> tree(t);
 
-    for (int i = 0; i < 10; ++i){
-        in >> t;
-        btree.insert(t);
+    int key;
+
+    while (in >> key) {
+        tree.insert(key);
     }
-//
-//    while (in >> t) {
-//        btree.insert(t);
-//    }
-    btree.print(out);
+
+    tree.print(out);
 }
+
