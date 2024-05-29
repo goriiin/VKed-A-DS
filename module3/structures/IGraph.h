64ba298e52d883c7e4b8c69d1e91a0a89a269cf1
dmitry <dkoshenkofff@mail.ru>
2024-05-30 02:00:00 +0300

    interface

---
+++ module3/structures/IGraph.h
@@ -0,0 +1,23 @@
+//
+// Created by dmitry on 29.05.2024.
+//
+
+#ifndef MODULE3_IGRAPH_H
+#define MODULE3_IGRAPH_H
+
+#include <vector>
+
+struct IGraph {
+    virtual ~IGraph() {}
+
+    // Добавление ребра от from к to.
+    virtual void AddEdge(int from, int to) = 0;
+
+    virtual int VerticesCount() const  = 0;
+
+    [[nodiscard]] virtual std::vector<int> GetNextVertices(int vertex) const = 0;
+    [[nodiscard]] virtual std::vector<int> GetPrevVertices(int vertex) const = 0;
+};
+
+
+#endif //MODULE3_IGRAPH_H
