e8b96ad00bcebe23ba95357b6bab2f5b577f8a6c
dmitry <dkoshenkofff@mail.ru>
2024-06-05 20:04:57 +0300

    add namespace

--- module3/task5/box_muller.h
+++ module3/task5/box_muller.h
@@ -9,40 +9,44 @@
 #include <random>
 #include "sd/edge/edge.h"
 
-using pdd = std::pair<double, double>;
 
-pdd get_point() {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_real_distribution<double> dis(0.0, 1.0);
+namespace point {
+    using pdd = std::pair<double, double>;
 
-    double u = dis(gen);
-    double v = dis(gen);
+    pdd get_point() {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        static std::uniform_real_distribution<double> dis(0.0, 1.0);
 
-    double a = std::sqrt(-2.0 * std::log(v));
-    double b = 2.0 * M_PI * u;
+        double u = dis(gen);
+        double v = dis(gen);
 
-    double x = a * std::cos(b);
-    double y = a * std::sin(b);
+        double a = std::sqrt(-2.0 * std::log(v));
+        double b = 2.0 * M_PI * u;
 
-    return std::make_pair(x, y);
-}
+        double x = a * std::cos(b);
+        double y = a * std::sin(b);
 
-double get_weight(pdd a, pdd b) {
-    return std::sqrt(std::pow((a.first - b.first), 2) + std::pow((a.second - b.second), 2));
-}
-
-std::vector<pdd> get_points(int count_points) {
-    std::vector<pdd> points;
-    points.reserve(count_points);
-    for (int i = 0; i < count_points; ++i) {
-        points.emplace_back(get_point());
+        return std::make_pair(x, y);
     }
-    return points;
+
+    std::vector<pdd> get_points(int count_points) {
+        std::vector<pdd> points;
+        points.reserve(count_points);
+        for (int i = 0; i < count_points; ++i) {
+            points.emplace_back(get_point());
+        }
+        return points;
+    }
+
+    double get_weight(const pdd &a, const pdd &b) {
+        return std::sqrt(std::pow((a.first - b.first), 2) + std::pow((a.second - b.second), 2));
+    }
+
+    double get_weight(const std::vector<pdd> &points, int from, int to) {
+        return ((from != to) ? get_weight(points[from], points[to]) : 0);
+    }
 }
 
-double get_weight(std::vector<pdd> points, int from, int to) {
-    return get_weight(points[from], points[to]);
-}
 
 #endif //TASK5_BOX_MULLER_H
